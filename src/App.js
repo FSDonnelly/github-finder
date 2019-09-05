692b6cb0ed69b27437f3264832b86489dccc76df
FSDonnelly <donnelly_frank@yahoo.com>
2019-09-04 21:20:58 -0700

    add User component passing props to UserItem mapping over users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
-import UserItem from './users/UserItem';
+import Users from './users/Users';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <div className='App'>
         <Navbar />
-        <UserItem />
+        <Users />
       </div>
     );
   }
